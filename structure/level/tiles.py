792cc9952e2f0e874890106cdc1297dedcafe9db
a <a.gariaf@udc.es>
2023-03-10 16:59:23 +0100

    Implement bitmasking "correctly"

--- structure/level/tiles.py
+++ structure/level/tiles.py
@@ -52,6 +52,11 @@ class TileMapper():
         obst2_spritesheet = SpriteSheet(pg.image.load('../sprites/environment_tileset/obstacles2.png'))
         obst2_sprites = obst2_spritesheet.load_tiled_style((16,16))
         self.obst2_sprite_pool = obst2_sprites[10]
+        
+        self.rock_dict = {"center": obst2_sprites[8], "left":obst2_sprites[8], "right":obst2_sprites[8],
+                     "top":obst2_sprites[2], "topleft_inner": obst2_sprites[2], "topleft_outer": obst2_sprites[2], "topright_inner": obst2_sprites[2], "topright_outer": obst2_sprites[2],
+                     "bottom": obst2_sprites[14], "bottomleft_inner": obst2_sprites[14], "bottomleft_outer": obst2_sprites[14], "bottomright_inner": obst2_sprites[14], "bottomright_outer": obst2_sprites[14]}
+
 
 
     def hardcoded_example(self) -> pg.Surface:
@@ -69,48 +74,8 @@ class TileMapper():
             return self.correspondence[tile_num][rand]
         
         return self.correspondence[tile_num]
-
-    '''
-    This generates a tuple of clockwise checks if the same tile continues (starting at 12 o'clock)
-    '''
-    def __get_bitmask(self, map_matrix, map_pos):
-        r, c = map_pos[0], map_pos[1]
-        value = map_matrix[r][c]
    
-        res = None
-        if r == 0: 
-            if c == 0:
-                res = (False, False, map_matrix[r][c+1]==value, map_matrix[r+1][c+1]==value, 
-                    map_matrix[r+1][c]==value, False, False, False)
-            elif c == len(map_matrix[0])-1:
-                res = (False, False, False, False, map_matrix[r+1][c]==value,
-                    map_matrix[r+1][c-1]==value, map_matrix[r][c-1]==value, False)
-            else:
-                res = (False, False, map_matrix[r][c+1]==value, map_matrix[r+1][c+1]==value,
-                     map_matrix[r+1][c]==value, map_matrix[r+1][c-1]==value, map_matrix[r][c-1]==value, False)
-        elif r == len(map_matrix)-1:
-            if c == 0:
-                res = (map_matrix[r-1][c]==value, map_matrix[r-1][c+1]==value, map_matrix[r][c+1]==value, False,
-                     False, False, False, False)
-            elif c == len(map_matrix[0])-1:
-                res = (map_matrix[r-1][c]==value, False, False, False, False,
-                    False, map_matrix[r][c-1]==value, map_matrix[r-1][c-1]==value)
-            else:
-                res = (map_matrix[r-1][c]==value, map_matrix[r-1][c+1]==value, map_matrix[r][c+1]==value, False, 
-                    False, False, map_matrix[r][c-1]==value, map_matrix[r-1][c-1]==value)
-        else:
-            if c == 0:
-                res = (map_matrix[r-1][c]==value, map_matrix[r-1][c+1]==value, map_matrix[r][c+1]==value,
-                     map_matrix[r+1][c+1]==value, map_matrix[r+1][c]==value, False, False, False)
-            elif c == len(map_matrix[0])-1:
-                res = (map_matrix[r-1][c]==value, False, False, False, map_matrix[r+1][c]==value,
-                    map_matrix[r+1][c-1]==value, map_matrix[r][c-1]==value, map_matrix[r-1][c-1]==value)
-            else:
-                res = (map_matrix[r-1][c]==value, map_matrix[r-1][c+1]==value, map_matrix[r][c+1]==value, map_matrix[r+1][c+1]==value,
-                     map_matrix[r+1][c]==value, map_matrix[r+1][c-1]==value, map_matrix[r][c-1]==value, map_matrix[r-1][c-1]==value)
-        return res 
-
-    
+ 
     def draw_lines(self, bitmask_dict, surf_size, sprite_size, surf, line):
         if line == 'top':
             pos = lambda x: (x*sprite_size[1], 0)
@@ -125,96 +90,115 @@ class TileMapper():
         for x in range(1, int(surf_size[0] / sprite_size[0])-1):
             surf.blit(bitmask_dict[line], pos(x))
 
-    def tile_bitmasking(self, map_matrix, map_pos: tuple[int,int], bitmask_dict: dict[str, pg.Surface], surf_size:tuple[int,int], sprite_size:tuple[int,int]):
+
+    def calculate_bitmask(self, map_matrix, map_pos):
+        r, c = map_pos[0], map_pos[1]
+        value = map_matrix[r][c]
+        
+        bitmask = 0b0
+
+        if r > 0:
+            #top
+            bitmask += (map_matrix[r-1][c] == value) * pow(2,0)
+            if c < len(map_matrix[0])-1:
+            #topright
+                bitmask += (map_matrix[r-1][c+1] == value) * pow(2,1)
+        if c < len(map_matrix[0])-1:
+            #right
+            bitmask += (map_matrix[r][c+1] == value) * pow(2,2)
+            if r < len(map_matrix[1])-1:
+            #bottomright
+                bitmask += (map_matrix[r+1][c+1] == value) * pow(2,3)
+        if r < len(map_matrix[1])-1:
+            #bottom
+            bitmask += (map_matrix[r+1][c] == value) * pow(2,4)
+            if c > 0:
+            #bottomleft
+                bitmask += (map_matrix[r+1][c-1] == value) * pow(2,5)
+        if c > 0:
+            #left
+            bitmask += (map_matrix[r][c-1] == value) * pow(2,6)
+            if r > 0:
+            #topleft
+                bitmask += (map_matrix[r-1][c-1] == value) * pow(2,7)
+        
+        # return a number that carries the following information in binary
+        # topleft: 0 left: 0 bottomleft: 0 bottom: 0 bottomright: 0 right: 0 topright: 0 top: 0 
+        return bitmask
+    
+    def tile_bitmasking(self, map_matrix, map_pos:tuple[int,int], bitmask_dict: dict[str, pg.Surface], surf_size:tuple[int,int], sprite_size:tuple[int,int]):
         r, c = map_pos[0], map_pos[1]
         value = map_matrix[r][c]
 
         surf = pg.Surface(surf_size, pg.SRCALPHA, 32)
-       
-        #deals with map_matrix borders
-        res = self.__get_bitmask(map_matrix, map_pos)
+        
+        bitmask = self.calculate_bitmask(map_matrix, map_pos)
 
-                
         #fill the surface with the default 
         for i in range(int(surf_size[0] / sprite_size[0])):
             for j in range(int(surf_size[1] / sprite_size[1])):
+                #maybe add some randomization here
                 surf.blit(bitmask_dict['center'], (j*sprite_size[1], i*sprite_size[0]))
 
-        #determine what to draw on the topleft corner 
-        if not res[6] and not res[0]:
+        #determine what to draw on the topleft corner
+        if (bitmask & 0b1) == 0 and (bitmask & 0b1000000) == 0 :
             surf.blit(bitmask_dict['topleft_inner'], (0,0))
-            #self.draw_top_line(bitmask_dict, surf_size, sprite_size, surf)
-            #self.draw_left_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'top')
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'left')
-        elif not res[0]:
+        elif (bitmask & 0b1) == 0:
             surf.blit(bitmask_dict['top'], (0,0))
-            #self.draw_top_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'top')
-        elif not res[6]:
+        elif (bitmask & 0b1000000) == 0:
             surf.blit(bitmask_dict['left'], (0,0))
-            #self.draw_left_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'left')
-        elif not res[7]:
+        elif (bitmask & 0b10000000) == 0:
             surf.blit(bitmask_dict['topleft_outer'], (0,0))
-        #else:
-            #surf.blit(bitmask_dict['center'], (0,0))
 
         #topright corner
-        if not res[0] and not res[2]:
+        if (bitmask & 0b1) == 0 and (bitmask & 0b100) == 0:
             surf.blit(bitmask_dict['topright_inner'], (surf_size[1]-sprite_size[1], 0))
-            #self.draw_top_line(bitmask_dict, surf_size, sprite_size, surf)
-            #self.draw_right_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'top')
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'right')
-        elif not res[0]:
+        elif (bitmask & 0b1) == 0:
             surf.blit(bitmask_dict['top'], (surf_size[1]-sprite_size[1], 0))
-            #self.draw_top_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'top')
-        elif not res[2]:
+        elif (bitmask & 0b100) == 0:
             surf.blit(bitmask_dict['right'], (surf_size[1]-sprite_size[1], 0))
-            #self.draw_right_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'right')
-        elif not res[1]:
+        elif (bitmask & 0b10) == 0:
             surf.blit(bitmask_dict['topright_outer'], (surf_size[1]-sprite_size[1], 0))
 
         #bottomright
-        if not res[2] and not res[4]:
+        if (bitmask & 0b100) == 0 and (bitmask & 0b10000) == 0:
             surf.blit(bitmask_dict['bottomright_inner'], (surf_size[1]-sprite_size[1], surf_size[0]-sprite_size[0]))
-            #self.draw_right_line(bitmask_dict, surf_size, sprite_size, surf)
-            #self.draw_bottom_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'right')
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'bottom')
-        elif not res[2]:
+        elif (bitmask & 0b100) == 0:
             surf.blit(bitmask_dict['right'], (surf_size[1]-sprite_size[1], surf_size[0]-sprite_size[0]))
-            #self.draw_right_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'right')
-        elif not res[4]:
+        elif (bitmask & 0b10000) == 0:
             surf.blit(bitmask_dict['bottom'], (surf_size[1]-sprite_size[1], surf_size[0]-sprite_size[0]))
-            #self.draw_bottom_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'bottom')
-        elif not res[3]:
+        elif (bitmask & 0b1000) == 0:
             surf.blit(bitmask_dict['bottomright_outer'], (surf_size[1]-sprite_size[1], surf_size[0]-sprite_size[0]))
 
         #bottomleft
-        if not res[4] and not res[6]:
+        if (bitmask & 0b10000) == 0 and (bitmask & 0b1000000) == 0:
             surf.blit(bitmask_dict['bottomleft_inner'], (0, surf_size[0]-sprite_size[0]))
-            #self.draw_left_line(bitmask_dict, surf_size, sprite_size, surf)
-            #self.draw_bottom_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'left')
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'bottom')
-        elif not res[4]:
+        elif (bitmask & 0b10000) == 0:
             surf.blit(bitmask_dict['bottom'], (0, surf_size[0]-sprite_size[0]))
-            #self.draw_bottom_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'bottom')
-        elif not res[6]: 
+        elif (bitmask & 0b1000000) == 0: 
             surf.blit(bitmask_dict['left'], (0, surf_size[0]-sprite_size[0]))
-            #self.draw_left_line(bitmask_dict, surf_size, sprite_size, surf)
             self.draw_lines(bitmask_dict, surf_size, sprite_size, surf, 'left')
-        elif not res[5]:
+        elif (bitmask & 0b100000) == 0:
             surf.blit(bitmask_dict['bottomleft_outer'], (0, surf_size[0]-sprite_size[0]))
 
         return surf
+
+ 
 
     def generate_random_surf(self, sprite_pool: list[pg.Surface], sprite_size: tuple[int,int], surf_size: tuple[int,int], scale=1):
         surf = pg.Surface(surf_size, pg.SRCALPHA, 32)
@@ -239,7 +223,12 @@ class TileMapper():
                     tile_surf = self.tile_bitmasking(map_matrix, (row_idx, col_idx), self.water_dict, size_per_tile, sprite_size)
                     map_surf.blit(tile_surf, (col_idx*size_per_tile[0], row_idx*size_per_tile[1]))
                 elif value == TileEnum.OBSTACLE_2.value:
-                    tile_surf = pg.transform.scale(self.obst2_sprite_pool, size_per_tile)
+                    #tile_surf = pg.transform.scale(self.obst2_sprite_pool, size_per_tile)
+                    #map_surf.blit(tile_surf, (col_idx*size_per_tile[0], row_idx*size_per_tile[1]))
+                    rocks = self.tile_bitmasking(map_matrix, (row_idx, col_idx), self.rock_dict, size_per_tile, sprite_size)
+                    tile_surf = self.generate_random_surf(self.ground_sprite_pool, sprite_size, size_per_tile)
+                    tile_surf.blit(rocks, (0,0))
+
                     map_surf.blit(tile_surf, (col_idx*size_per_tile[0], row_idx*size_per_tile[1]))
                 elif value == TileEnum.SPAWN.value:
                     pg.draw.rect(map_surf, (255,0,0), pg.Rect(col_idx*size_per_tile[0], row_idx*size_per_tile[1],size_per_tile[1],size_per_tile[0])) 
