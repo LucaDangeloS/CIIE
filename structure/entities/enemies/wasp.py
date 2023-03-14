b8b298e9fbf8711d3d485404edfd2554b02cdbc9
LucaDangeloS <lucad7@gmail.com>
2023-03-14 03:01:04 +0100

    Implemented Wasp enemy class

--- structure/entities/enemies/wasp.py
+++ structure/entities/enemies/wasp.py
@@ -1,9 +1,11 @@
+from typing import List
 from entities.enemy import Enemy
+from entities.enemies.behaviour import ChaseBehavior
 
 
 class Wasp(Enemy):
 
-    def __init__(self, collision_sprites, sprite_path, entity_rect, sprite_scale=1):
-        super.__init__(collision_sprites, sprite_path, entity_rect, sprite_scale)
-        self.state = 'idle'
-        self.bahavior = MeleeBehaviour()
+    def __init__(self, collision_sprites, damageable_sprites: List, sprite_path, entity_rect, sprite_scale=1):
+        super().__init__(collision_sprites, damageable_sprites, sprite_path, entity_rect, sprite_scale)
+        self.behavior = ChaseBehavior(self, 300)
+
