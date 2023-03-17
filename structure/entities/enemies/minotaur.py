50460808c76ca52b2438e1640f46e5d20cf276df
LucaDangeloS <lucad7@gmail.com>
2023-03-17 17:31:00 +0100

    Implemented Minotaur (broken fr)

--- structure/entities/enemies/minotaur.py
+++ structure/entities/enemies/minotaur.py
@@ -4,14 +4,14 @@ from entities.enemies.behaviour import ChaseBehavior
 from pygame import Rect
 from weapons.monster import MonsterWeapon
 
-class Wasp(Enemy):
+class Minotaur(Enemy):
 
     def __init__(self, collision_sprites, damageable_sprites: List, pos, sprite_scale=1):
-        entity_rect = Rect(pos[0], pos[1], 16, 16)
-        sprite_path = '../sprites/enemies/wasp'
-        super().__init__(collision_sprites, damageable_sprites, sprite_path, entity_rect, sprite_scale, facing_sprites='left')
-        self.health = 2
-        self.behavior = ChaseBehavior(self, 300, 40)
+        entity_rect = Rect(pos[0], pos[1], 64, 45)
+        sprite_path = '../sprites/enemies/minotaur'
+        super().__init__(collision_sprites, damageable_sprites, sprite_path, entity_rect, sprite_scale, facing_sprites='right')
+        self.health = 5
+        self.behavior = ChaseBehavior(self, 300, 60)
 
         # Wasp specific "weapon"
         half_size = (self.rect.size[0] / 2, self.rect.size[1] / 2)
