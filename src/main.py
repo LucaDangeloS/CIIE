bf9ee1c61e67fffc9bb704ab40ee2f68404c3fb7
LucaDangeloS <lucad7@gmail.com>
2023-03-20 16:19:07 +0100

    Change annoying music in levels

--- src/main.py
+++ src/main.py
@@ -30,9 +30,9 @@ screen_res = director.screen.get_size()
 #Buttons for initial menu:
 x, y = (screen_res[0] // 2) - 48*3, screen_res[1] // 3
 
-director.push_scene((level_3, "music.ogg")) # TODO: change music
-director.push_scene((level_2, "music.ogg")) # TODO: change music
-director.push_scene((level, "music.ogg"))
+director.push_scene((level_3, "level1Music.ogg")) # TODO: change music
+director.push_scene((level_2, "level1Music.ogg")) # TODO: change music
+director.push_scene((level, "level1Music.ogg"))
 
 # Menu buttons
 btt_play = Button(director.pop_scene, pg.image.load('../sprites/menu/buttons.png'), (0, 0,97,41), Rect(x,y,200,100), 3)
